59e4173c3e77ea417a1d53b9d5698bb03a445d65
Son Nguyen <nhuson2306@gmail.com>
2023-10-03 22:44:50 +0700

    Section 49: Xử lý message khi gặp sự cố MQ server die (TTL)

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 'use strict';
 
-const { consumerToQueue } = require('./src/services/consumerQueue.service');
+const { consumerToQueue, consumerToQueueNormal, consumerToQueueFailed } = require('./src/services/consumerQueue.service');
 
 const queueName = 'test-topic';
 
-consumerToQueue(queueName)
+// consumerToQueue(queueName)
+//     .then(() => {
+//         console.log(`Message consumer started ${queueName}`);
+//     })
+//     .catch((error) => {
+//         console.error('Message consumer error:: ', error.message);
+//     });
+
+consumerToQueueNormal(queueName)
     .then(() => {
-        console.log(`Message consumer started ${queueName}`);
+        console.log(`Message consumer normal started`);
     })
     .catch((error) => {
-        console.error('Message consumer error:: ', error.message);
+        console.error('Message consumer normal error:: ', error.message);
+    });
+
+consumerToQueueFailed(queueName)
+    .then(() => {
+        console.log(`Message consumer failed started`);
+    })
+    .catch((error) => {
+        console.error('Message consumer failed error:: ', error.message);
     });
--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -11,6 +11,54 @@ const messageService = {
             console.error(`Error consumer to queue:: `, error);
         }
     },
+    // Case processing
+    consumerToQueueNormal: async () => {
+        try {
+            const { channel } = await connectToRabbitMQ();
+            const notificationQueue = 'notificationQueueProcess'; // assertQueue
+
+            const timeExpired = 15000;
+            setTimeout(() => {
+                channel.consume(notificationQueue, (msg) => {
+                    console.log(`SEND NOTIFICATION SUCCESSFULLY PROCESSED::`, msg.content.toString());
+                    channel.ack(msg);
+                });
+            }, timeExpired);
+        } catch (error) {
+            console.error(`ConsumerToQueueNormal error:: `, error.message);
+        }
+    },
+    // Case failed processing
+    consumerToQueueFailed: async () => {
+        try {
+            const { channel } = await connectToRabbitMQ();
+
+            const notificationExchangeDLX = 'notificationExDLX'; // notificationExDLX direct
+            const notificationRoutingKeyDLX = 'notificationRoutingKeyDLX';
+            const notificationHandler = 'notificationQueueHotFix';
+
+            await channel.assertExchange(notificationExchangeDLX, 'direct', {
+                durable: true,
+            });
+
+            const queueResult = await channel.assertQueue(notificationHandler, {
+                exclusive: false,
+            });
+
+            await channel.bindQueue(queueResult.queue, notificationExchangeDLX, notificationRoutingKeyDLX);
+            await channel.consume(
+                queueResult.queue,
+                (msgFailed) => {
+                    console.log(`This notification error, pls hot fix::`, msgFailed.content.toString());
+                },
+                {
+                    noAck: true,
+                }
+            );
+        } catch (error) {
+            console.error(`ConsumerToQueueFailed error:: `, error.message);
+        }
+    },
 };
 
 module.exports = messageService;
